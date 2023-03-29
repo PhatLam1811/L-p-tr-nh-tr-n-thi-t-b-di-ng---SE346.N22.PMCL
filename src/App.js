c1a24468d6debfafa3390c0a5596ca8e71750215
phatlam1811 <phatlam1811@gmail.com>
2023-03-29 08:53:14 +0700

    implement structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import Homework_4_App from "./src/components/Homework_4_App.js";
-import counterReducer from "./src/redux/counterReducer.js";
+import Homework_4_App from "./components/Homework_4_App.js";
+import counterReducer from "./redux/counterReducer.js";
 
 const store = createStore(counterReducer);
 
