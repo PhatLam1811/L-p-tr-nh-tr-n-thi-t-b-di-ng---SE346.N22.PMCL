d47b267ced3ee03823e571fea2cd35daa0b064c5
phatlam1811 <phatlam1811@gmail.com>
2023-07-01 11:29:25 +0700

    fix main screen UI

--- App.js
+++ App.js
@@ -1,26 +1,17 @@
 /* eslint-disable*/
 
-import React, { useState, useEffect } from "react";
-import Task from "./src/components/tasks/Task"
-import { View, Text, StyleSheet, KeyboardAvoidingView, Platform, TouchableOpacity, Button, Keyboard } from 'react-native'
-import { TextInput } from "@react-native-material/core";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SaveNoteAction } from './src/actions/SaveNote'
-import { GetAllNoteAction, GetNoteAction } from './src/actions/GetNote'
-import { DeleteAllNoteAction, DeleteNoteAction } from './src/actions/DeleteNote'
-import NewTaskScreen from "./src/screen/NewTaskScreen";
+import React from "react";
+
 import MainScreen from "./src/screen/MainScreen";
+import NewTaskScreen from "./src/screen/NewTaskScreen";
 import DetailScreen from "./src/screen/DetailScreen";
-import { ScreenType } from "./src/constants/Constants"
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 const Stack = createNativeStackNavigator();
 
-
-
 const App = () => {
-
   const onSave = () => {
     console.log('save on App.js')
   }
--- src/components/notes/NoteList.js
+++ src/components/notes/NoteList.js
@@ -1,44 +1,43 @@
-import React,{useState,useEffect}  from "react";
+import React, { useState, useEffect } from "react";
 
-import { Text, TouchableOpacity,View } from "react-native";
-import {DeleteAllNoteAction, DeleteNoteAction}from'./../../actions/DeleteNote'
+import { Text, TouchableOpacity, View } from "react-native";
+import { DeleteAllNoteAction, DeleteNoteAction } from './../../actions/DeleteNote'
 
 import Task from "./../tasks/Task"
 
 const NoteList = (props) => {
+  const [notes, setNotes] = useState([]);
 
-    const [notes,setNotes] = useState([]);
+  const openTask = async (index) => {
+    // console.log("index is being deleted:" + index);
+    // let itemsCopy = [...notes];
+    // itemsCopy.splice(index,1); 
+    // setNotes([...itemsCopy]);
+    // await DeleteNoteAction(notes[index].ID);
 
-    const openTask = async(index)  =>{
-        // console.log("index is being deleted:" + index);
-        // let itemsCopy = [...notes];
-        // itemsCopy.splice(index,1); 
-        // setNotes([...itemsCopy]);
-        // await DeleteNoteAction(notes[index].ID);
+    props.screenNavigation(notes[index].ID);
+  }
 
-        props.screenNavigation(notes[index].ID);
-      }
+  useEffect(() => {
+    console.log('porops list is: ');
+    console.log(props.list)
+    setNotes(props.list);
+  }, [props]);
 
-    useEffect(() => {
-        console.log('porops list is: ');
-        console.log(props.list)
-        setNotes(props.list);
-    }, [props]);
-
-    return (
-        <View style={props.style}>
-            <Text style={{ color: "black" }}> This is a note list</Text>
-            <View>
-{            notes.map((item, index) => {
-              return (
-                <TouchableOpacity key={index} onPress={() => openTask(index)}>
-                  <Task text={item.noteData}/>
-                </TouchableOpacity>
-              )
-            })}
-            </View>
-        </View >
-    );
+  return (
+    <View style={props.style}>
+      <Text style={{ color: "black" }}> This is a note list</Text>
+      <View>
+        {notes.map((item, index) => {
+          return (
+            <TouchableOpacity key={index} onPress={() => openTask(index)}>
+              <Task text={item.noteData} />
+            </TouchableOpacity>
+          )
+        })}
+      </View>
+    </View >
+  );
 }
 
 export default NoteList;
